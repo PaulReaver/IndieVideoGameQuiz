45cde401b84abfe9420f1caf475d0538fb0ef920
Pavlitos <vallasff@hotmail.com>
2018-01-22 12:04:20 +0200

    Indie Video Game Quiz

--- app/src/main/java/com/example/android/videogamequiz/MainActivity.java
+++ app/src/main/java/com/example/android/videogamequiz/MainActivity.java
@@ -7,15 +7,111 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    /*Declaration of variables*/
+    static final String KEY_SCORE = "";
+    RadioGroup limboRadioGroup;
+    RadioGroup journeyRadioGroup;
+    RadioButton correctAnswer1;
+    RadioButton correctAnswer2;
+    CheckBox wrongAnswer3;
+    CheckBox correctAnswer3A;
+    CheckBox correctAnswer3B;
+    CheckBox wrongAnswer4;
+    CheckBox correctAnswer4A;
+    CheckBox correctAnswer4B;
+    EditText nameField;
+    EditText correctAnswer5;
+    EditText correctAnswer6;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Passing the saved state value to the score variable and displays it.*/
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(KEY_SCORE);
+            displayScore(score);
+        }
+
+        /*Assigns RadioButtons CheckBoxes and EditTexts to corresponding variables*/
+        limboRadioGroup = (RadioGroup) findViewById(R.id.limboRadioGroup);
+        journeyRadioGroup = (RadioGroup) findViewById(R.id.journeyRadioGroup);
+        correctAnswer1 = (RadioButton) findViewById(R.id.correctAnswer1);
+        correctAnswer2 = (RadioButton) findViewById(R.id.correctAnswer2);
+        wrongAnswer3 = (CheckBox) findViewById(R.id.wrongAnswer3);
+        correctAnswer3A = (CheckBox) findViewById(R.id.correctAnswer3A);
+        correctAnswer3B = (CheckBox) findViewById(R.id.correctAnswer3B);
+        wrongAnswer4 = (CheckBox) findViewById(R.id.wrongAnswer4);
+        correctAnswer4A = (CheckBox) findViewById(R.id.correctAnswer4A);
+        correctAnswer4B = (CheckBox) findViewById(R.id.correctAnswer4B);
+        nameField = (EditText) findViewById(R.id.nameField);
+        correctAnswer5 = (EditText) findViewById(R.id.correctAnswer5);
+        correctAnswer6 = (EditText) findViewById(R.id.correctAnswer6);
+    }
+
+    /*Saving the instance when screen rotates*/
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(KEY_SCORE, score);
+
+        // call superclass to save any view hierarchy
+        super.onSaveInstanceState(outState);
+    }
+
+    /*Displays the score.*/
+    public void displayScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.scoreView);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /*Clears all answers*/
+    public void clearAnswers() {
+
+        /*Clears 1st question's answer */
+        limboRadioGroup.clearCheck();
+
+        /*Clears 2nd question's answer */
+        journeyRadioGroup.clearCheck();
+
+        /*Clears 3rd question's answer */
+        if (wrongAnswer3.isChecked()) {
+            wrongAnswer3.setChecked(false);
+        }
+
+        if (correctAnswer3A.isChecked()) {
+            correctAnswer3A.setChecked(false);
+        }
+
+        if (correctAnswer3B.isChecked()) {
+            correctAnswer3B.setChecked(false);
+        }
+
+        /*Clears 4th question's answer */
+        if (wrongAnswer4.isChecked()) {
+            wrongAnswer4.setChecked(false);
+        }
+
+        if (correctAnswer4A.isChecked()) {
+            correctAnswer4A.setChecked(false);
+        }
+
+        if (correctAnswer4B.isChecked()) {
+            correctAnswer4B.setChecked(false);
+        }
+
+        /*Clears 5th question's answer */
+        correctAnswer5.setText("");
+
+        /*Clears 6th question's answer */
+        correctAnswer6.setText("");
     }
 
     /*Method is called the submit button is clicked*/
@@ -24,34 +120,16 @@ public class MainActivity extends AppCompatActivity {
         /*Declaration of variables*/
         boolean nameIsValid = true;
         int rightAnswers = 0;
+        score = 0;
         String message = "";
 
         /*Gets the user's name and passes it in a variable*/
-        EditText nameField = (EditText) findViewById(R.id.nameField);
         String name = nameField.getText().toString();
 
-        /*Gets the answer from question 1*/
-        RadioButton correctAnswer1 = (RadioButton) findViewById(R.id.correctAnswer1);
-
-        /*Gets the answer from question 2*/
-        RadioButton correctAnswer2 = (RadioButton) findViewById(R.id.correctAnswer2);
-
-        /*Gets the answers from question 3*/
-        CheckBox wrongAnswer3 = (CheckBox) findViewById(R.id.wrongAnswer3);
-        CheckBox correctAnswer3A = (CheckBox) findViewById(R.id.correctAnswer3A);
-        CheckBox correctAnswer3B = (CheckBox) findViewById(R.id.correctAnswer3B);
-
-        /*Gets the answers from question 4*/
-        CheckBox wrongAnswer4 = (CheckBox) findViewById(R.id.wrongAnswer4);
-        CheckBox correctAnswer4A = (CheckBox) findViewById(R.id.correctAnswer4A);
-        CheckBox correctAnswer4B = (CheckBox) findViewById(R.id.correctAnswer4B);
-
         /*Gets the answer from question 5*/
-        EditText correctAnswer5 = (EditText) findViewById(R.id.correctAnswer5);
         String answer5 = correctAnswer5.getText().toString();
 
         /*Gets the answer from question 6*/
-        EditText correctAnswer6 = (EditText) findViewById(R.id.correctAnswer6);
         String answer6 = correctAnswer6.getText().toString();
 
         /*Checks if the name field is empty and shows appropriate message*/
@@ -63,31 +141,37 @@ public class MainActivity extends AppCompatActivity {
         /*Checks if question 1 was answered correctly*/
         if (correctAnswer1.isChecked()) {
             rightAnswers += 1;
+            score += 400;
         }
 
         /*Checks if question 2 was answered correctly*/
         if (correctAnswer2.isChecked()) {
             rightAnswers += 1;
+            score += 500;
         }
 
         /*Checks if question 3 was answered correctly*/
         if (correctAnswer3A.isChecked() && correctAnswer3B.isChecked() && !wrongAnswer3.isChecked()) {
             rightAnswers += 1;
+            score += 800;
         }
 
         /*Checks if question 4 was answered correctly*/
         if (correctAnswer4A.isChecked() && correctAnswer4B.isChecked() && !wrongAnswer4.isChecked()) {
             rightAnswers += 1;
+            score += 1000;
         }
 
         /*Checks if question 5 was answered correctly*/
-        if (answer5.equals("Mugman")) {
+        if (answer5.equalsIgnoreCase("Mugman")) {
             rightAnswers += 1;
+            score += 1500;
         }
 
         /*Checks if question 6 was answered correctly*/
-        if (answer6.equals("Vellie")) {
+        if (answer6.equalsIgnoreCase("Vellie")) {
             rightAnswers += 1;
+            score += 1800;
         }
 
         /*Text if 0 questions were answered correctly*/
@@ -116,6 +200,11 @@ public class MainActivity extends AppCompatActivity {
         if (nameIsValid) {
             /*Toast message with the results*/
             Toast.makeText(this, name + " " + message, Toast.LENGTH_LONG).show();
+            displayScore(score);
+            clearAnswers();
+        } else {
+            score = 0;
+            displayScore(score);
         }
     }
 }
